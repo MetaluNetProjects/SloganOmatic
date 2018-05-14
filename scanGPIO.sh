bda12559900756d84eabf297c2a2a6db370aac54
Antoine Rousseau <_antoine_@metalu.net>
2018-05-14 13:12:10 +0000

    pin numbers update to scanGPIO

--- scanGPIO.sh
+++ scanGPIO.sh
@@ -3,22 +3,20 @@
 # sudo ln -s /home/pi/Documents/SloganOmatic/pdGPIO.service /etc/systemd/system/
 # run: sudo systemctl start pdGPIO.service
 
-pin[1]=5
-pin[2]=6
-pin[3]=13
-pin[4]=19
-pin[5]=26
+pin[1]=4
+pin[2]=17
+pin[3]=27
+pin[4]=22
 
 b[1]=0
 b[2]=0
 b[3]=0
 b[4]=0
-b[5]=0
 
-for n in 1 2 3 4 5; do gpio -g mode ${pin[$n]} up ; done
+for n in 1 2 3 4; do gpio -g mode ${pin[$n]} up ; done
 
 while true ; do
-	for n in 1 2 3 4 5; do 
+	for n in 1 2 3 4; do 
 	 	button=`gpio -g read ${pin[$n]}`
 		if [ $button != ${b[$n]} ] ; then
 			b[$n]=$button
